4573bb76ab6ca57e4e052206fbd967a21cf3aba3
Serakyuth  Ly <159991489+Brotheryuth@users.noreply.github.com>
2026-07-04 04:57:01 +0700

    set up for deploy backend to ec2

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json()); // be able to handle http request
 
 const userRoute       = require("../src/routes/user.route");
